40d3fa670bdf78b09433ca7968baa5d521e6d822
Christabel <christabel.masakhwe@student.moringaschool.com>
2023-01-17 22:05:35 +0300

    Config hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 # config.hosts << "job-app-production.up.railway.app"
 
-config.hosts << "job-app-bbom.onrender.com/"
+config.hosts << "job-app-bbom.onrender.com"
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # config.hosts << "job-app-production.up.railway.app"
-  config.hosts << "job-app-bbom.onrender.com/"
+  config.hosts << "job-app-bbom.onrender.com"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
